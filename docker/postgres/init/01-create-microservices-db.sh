abc481294f61f3c71b723f664bd1ffd381410ed9
João Antônio Barbosa <joaobarbosadev@gmail.com>
2026-04-15 10:54:37 -0300

    feat: initialize order service with database and messaging configuration

---
+++ docker/postgres/init/01-create-microservices-db.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE order_db;
+    CREATE DATABASE payment_db;
+    CREATE DATABASE inventory_db;
+    CREATE DATABASE notification_db;
+EOSQL
